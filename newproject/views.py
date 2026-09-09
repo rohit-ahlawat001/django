ae2f96c14d023af9994f4c1f10c491d62db9e45a
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-09 18:46:06 +0530

    did django setup

---
+++ newproject/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def show_Response(request):
+    return HttpResponse("Your responce is here dhsdlkfdsjfldjfdfj; f;asjd;sladj")
+
+# Create your views here.
