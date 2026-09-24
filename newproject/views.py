3a828e4dde3350837736b90a4126549dda9d896f
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-24 22:18:21 +0530

    Add HTML home page response

--- newproject/views.py
+++ newproject/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show_Response(request):
-    return HttpResponse("Your responce is here dhsdlkfdsjfldjfdfj; f;asjd;sladj")
+    html = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Welcome to my Django page</h1>
+        <p>Your page is working.</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html)
 
 
